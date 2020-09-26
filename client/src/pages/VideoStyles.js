b7eb2ac36d3823e827c181bce3c6485ff2632c65
KaylingW <kwong028@fiu.edu>
2020-09-26 11:00:09 -0400

    Uh Oh

---
+++ client/src/pages/VideoStyles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const PageContainer = styled.div`
+width: 100%;
+display: flex;
+`;
+
+
+export const OrderContainer = styled.div`
+ justify-content: center;
+ align-items: center;
+ font-size: 16px;
+display: flex;
+width: 60%;
+margin-top: 5%;
+`;
