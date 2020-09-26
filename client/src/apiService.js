e29b6fc7e886be67a1cef4cffe15e10ff608494a
Denis Ortega <ortega.dc98@gmail.com>
2020-09-26 01:40:04 -0400

    api service for ocr

---
+++ client/src/apiService.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const api = axios.create({
+    baseURL: 'http://localhost:3001',
+  });
+
+const sendScreenshot = (dataUri) => {
+    api.post('/api/ocr', { image: dataUri })
+        .then(res => res.data);
+}
+
+export default {
+    sendScreenshot
+}
