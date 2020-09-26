01852fdcc24656f49cd08dd732a21a878abe6874
KaylingW <kwong028@fiu.edu>
2020-09-26 11:48:02 -0400

    UI fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Video from './pages/Video';
 const App = () => (
-<div style={{background:'linear-gradient(90deg, rgba(162,212,217,1) 0%, rgba(0,212,255,1) 100%)'}}>
+<div style={{height: '100vh',background:'linear-gradient(90deg, rgba(162,212,217,1) 0%, rgba(0,212,255,1) 100%)'}}>
  <Video></Video>
  </div>
 )
