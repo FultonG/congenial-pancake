99a7d0101f05fe1859f14dd29284ddbc68bab01b
Lazaro Hurtado <hurtadolazaro12@gmail.com>
2020-09-26 13:37:34 -0400

    create merchant with vendor signup

--- routes/finance/merchants.js
+++ routes/finance/merchants.js
@@ -1,36 +1,6 @@
 const router = require("express").Router();
-const Vendor = require("../../models/vendor");
 const axios = require("axios");
 const capitalAPI = process.env.CAPITALONE_KEY;
-
-router.post("/create", async (req, res) => {
-  let { user, username, password, ...payload } = req.body.merchant;
-
-  if (payload._id) {
-    return res
-      .status(400)
-      .send({ msg: "Merchant already created for this account" });
-  }
-
-  let merchantData = null;
-  await axios
-    .post(
-      `http://api.reimaginebanking.com/merchants?key=${capitalAPI}`,
-      payload
-    )
-    .then((response) => {
-      const { _id, ...data } = response.data.objectCreated;
-      merchantData = { ...data, merchant_id: _id };
-    })
-    .catch((err) => res.status(500).send({ err }));
-
-  console.log(merchantData);
-
-  await Vendor.updateOne({ username }, merchantData);
-  user = await Vendor.findOne({ username });
-
-  res.send(user);
-});
 
 router.get("/get/:id", async (req, res) => {
   const id = req.params.id;
